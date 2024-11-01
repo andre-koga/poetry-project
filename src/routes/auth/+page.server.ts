5f157ca4134c52d95a7c2807c6f16a9ad0c2f50c
Andre Koga <andrehkoga1@gmail.com>
2024-11-01 18:12:57 -0400

    retrying auth vercel

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -2,6 +2,18 @@ import { redirect } from '@sveltejs/kit'
 
 import type { Actions } from './$types'
 
+const getURL = () => {
+    let url =
+        process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+        process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+        'http://localhost:3000/'
+    // Make sure to include `https://` when not localhost.
+    url = url.startsWith('http') ? url : `https://${url}`
+    // Make sure to include a trailing `/`.
+    url = url.endsWith('/') ? url : `${url}/`
+    return url
+}
+
 export const actions: Actions = {
     signup: async ({ request, locals: { supabase } }) => {
         const formData = await request.formData()
@@ -33,11 +45,14 @@ export const actions: Actions = {
         const { data, error } = await supabase.auth.signInWithOAuth({
             provider: 'github',
             options: {
-                redirectTo: 'http://localhost:5173/auth/callback',
+                redirectTo: getURL(),
             },
         })
 
-        if (data.url) {
+        if (error) {
+            console.error(error)
+            redirect(303, '/auth/error')
+        } else if (data.url) {
             redirect(303, data.url) // use the redirect API for your server framework
         }
     }
